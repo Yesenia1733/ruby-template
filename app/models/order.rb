260e61c095aa6e75434570f0f75f9efc3bb70e60
eprcristian <eprcristian@gmail.com>
2022-08-02 23:09:42 -0400

    RelacionesModelos

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :equipment
   belongs_to :pre_order
+  has_many :services, through: :order_details
+  has_one :order, through: :equipment
+  has_many :warranties
+  has_many :stages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   enum role: [:enduser, :operator, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  has_many :equipment
+
   def set_default_role
     self.role ||= :enduser
   end
