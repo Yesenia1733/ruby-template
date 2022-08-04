c85b8ed2e52aa61250c5f343571b307bfc8f25f6
gabylini <mgabriela.rc@gmail.com>
2022-08-03 21:45:37 -0400

    add_layout_admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   root to: 'home#index'
   get 'pages/index'
-  devise_for :users
+  devise_for :users 
   devise_scope :user do
     authenticated :user do
       root 'home#index', as: :authenticated_root
